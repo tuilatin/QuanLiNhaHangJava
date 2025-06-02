aed4029b842445dd434540433d12f552a104af92
tuilatin <nguyentin1763@gmail.com>
2025-06-02 14:36:06 +0700

    tin

--- src/java/control/admin/AdminEDITKhachHang.java
+++ src/java/control/admin/AdminEDITKhachHang.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+//
 @WebServlet(name = "AdminEDITKhachHang", urlPatterns = {"/editKH"})
 public class AdminEDITKhachHang extends HttpServlet {
 
